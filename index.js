161926808766ec9f4c08e88645fe951be90688e8
Andrey <Uriaswift@gmail.com>
2021-05-12 16:05:23 +0500

    added info for new store, wondow 12.05.2021

--- index.js
+++ index.js
@@ -151,13 +151,146 @@ const dataInfo = [
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/932.jpg'
     }
 ]
+
+app.get('node/api/windowStore', (request, response) => {
+    const json = {
+        total: 20,
+        items: [
+            {   id: 1,
+                name: "John",
+                surname: "Fris",
+                phone: "5556194558",
+                address: 'Westside'
+            },
+            {
+                id: 2,
+                name: "Steve",
+                surname: "Jobs",
+                phone: "5557503664",
+                address: 'Eastside'
+            },
+            {   id: 3,
+                name: "Ram",
+                surname: "Kapoor",
+                phone: "5554063754",
+                address: 'Northside'
+            },
+            {   id: 4,
+                name: "Bill",
+                surname: "Gates",
+                phone: "5556816585",
+                address: 'Westside'
+            },
+            {   id: 5,
+                name: "Moin",
+                surname: "Khan",
+                phone: "5555875011",
+                address: 'Southside'
+            },
+            {   id: 6,
+                name: "Naruto",
+                surname: "Uzumaki",
+                phone: "5552875232",
+                address: 'Northside'
+            },
+            {   id: 7,
+                name: "Sasuke",
+                surname: "Uchiha",
+                phone: "5556727160",
+                address: 'Westside'
+            },
+            {   id: 8,
+                name: "Sakura",
+                surname: "Haruno",
+                phone: "5551125588",
+                address: 'Southside'
+            },
+            {   id: 9,
+                name: "Kakashi",
+                surname: "Hatake",
+                phone: "5559667564",
+                address: 'Southside'
+            },
+            {   id: 10,
+                name: "Itachi",
+                surname: "Uchiha",
+                phone: "5559987698",
+                address: 'Eastside'
+            },
+            {   id: 11,
+                name: "Kakashi",
+                surname: "Hatake",
+                phone: "5559667564",
+                address: 'Southside'
+            },
+            {   id: 12,
+                name: "Kakashi",
+                surname: "Hatake",
+                phone: "5559667564",
+                address: 'Southside'
+            },
+            {   id: 13,
+                name: "Kakashi",
+                surname: "Hatake",
+                phone: "5559667564",
+                address: 'Southside'
+            },
+            {   id: 14,
+                name: "Kakashi",
+                surname: "Hatake",
+                phone: "5559667564",
+                address: 'Southside'
+            },
+            {   id: 15,
+                name: "Kakashi",
+                surname: "Hatake",
+                phone: "5559667564",
+                address: 'Southside'
+            },
+            {   id: 16,
+                name: "Kakashi",
+                surname: "Hatake",
+                phone: "5559667564",
+                address: 'Southside'
+            },
+            {   id: 17,
+                name: "Kakashi",
+                surname: "Hatake",
+                phone: "5559667564",
+                address: 'Southside'
+            },
+            {   id: 18,
+                name: "Kakashi",
+                surname: "Hatake",
+                phone: "5559667564",
+                address: 'Southside'
+            },
+            {   id: 19,
+                name: "Kakashi",
+                surname: "Hatake",
+                phone: "5559667564",
+                address: 'Southside'
+            },
+            {   id: 20,
+                name: "Kakashi",
+                surname: "Hatake",
+                phone: "5559667564",
+                address: 'Southside'
+            }
+        ]
+    };
+    response.send(json);
+});
+
 app.get('/node/api/newStore', (req, res) => {
     const page = req.query.page;
     const limit = req.query.limit;
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
-    const result = dataInfo.slice(startIndex, endIndex);
-    res.json(result);
+    res.json({
+        total: dataInfo.length,
+        items: dataInfo.slice(startIndex, endIndex)
+    });
 });
 
 //wswsw
