6764f75fcb936f50015afa162b48bad4fecb408b
Andrey <Uriaswift@gmail.com>
2021-05-14 12:33:09 +0500

    treestore

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 //-----------------------------------------------------------
 const dataInfo = [
-    {   id: 1,
+    {
+        id: 1,
         name: "John",
         surname: "Fris",
         phone: "5556194558",
@@ -24,126 +25,144 @@ const dataInfo = [
         address: 'Eastside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/929.jpg'
     },
-    {   id: 3,
+    {
+        id: 3,
         name: "Ram",
         surname: "Kapoor",
         phone: "5554063754",
         address: 'Northside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/928.jpg'
     },
-    {   id: 4,
+    {
+        id: 4,
         name: "Bill",
         surname: "Gates",
         phone: "5556816585",
         address: 'Westside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/927.jpg'
     },
-    {   id: 5,
+    {
+        id: 5,
         name: "Moin",
         surname: "Khan",
         phone: "5555875011",
         address: 'Southside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/926.jpg'
     },
-    {   id: 6,
+    {
+        id: 6,
         name: "Naruto",
         surname: "Uzumaki",
         phone: "5552875232",
         address: 'Northside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/925.jpg'
     },
-    {   id: 7,
+    {
+        id: 7,
         name: "Sasuke",
         surname: "Uchiha",
         phone: "5556727160",
         address: 'Westside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/924.jpg'
     },
-    {   id: 8,
+    {
+        id: 8,
         name: "Sakura",
         surname: "Haruno",
         phone: "5551125588",
         address: 'Southside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/923.jpg'
     },
-    {   id: 9,
+    {
+        id: 9,
         name: "Kakashi",
         surname: "Hatake",
         phone: "5559667564",
         address: 'Southside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/922.jpg'
     },
-    {   id: 10,
+    {
+        id: 10,
         name: "Itachi",
         surname: "Uchiha",
         phone: "5559987698",
         address: 'Eastside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/921.jpg'
     },
-    {   id: 11,
+    {
+        id: 11,
         name: "Kakashi",
         surname: "Hatake",
         phone: "5559667564",
         address: 'Southside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/920.jpg'
     },
-    {   id: 12,
+    {
+        id: 12,
         name: "Kakashi",
         surname: "Hatake",
         phone: "5559667564",
         address: 'Southside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/919.jpg'
     },
-    {   id: 13,
+    {
+        id: 13,
         name: "Kakashi",
         surname: "Hatake",
         phone: "5559667564",
         address: 'Southside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/918.jpg'
     },
-    {   id: 14,
+    {
+        id: 14,
         name: "Kakashi",
         surname: "Hatake",
         phone: "5559667564",
         address: 'Southside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/917.jpg'
     },
-    {   id: 15,
+    {
+        id: 15,
         name: "Kakashi",
         surname: "Hatake",
         phone: "5559667564",
         address: 'Southside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/916.jpg'
     },
-    {   id: 16,
+    {
+        id: 16,
         name: "Kakashi",
         surname: "Hatake",
         phone: "5559667564",
         address: 'Southside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/917.jpg'
     },
-    {   id: 17,
+    {
+        id: 17,
         name: "Kakashi",
         surname: "Hatake",
         phone: "5559667564",
         address: 'Southside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/917.jpg'
     },
-    {   id: 18,
+    {
+        id: 18,
         name: "Kakashi",
         surname: "Hatake",
         phone: "5559667564",
         address: 'Southside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/917.jpg'
     },
-    {   id: 19,
+    {
+        id: 19,
         name: "Kakashi",
         surname: "Hatake",
         phone: "5559667564",
         address: 'Southside',
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/931.jpg'
     },
-    {   id: 20,
+    {
+        id: 20,
         name: "Kakashi",
         surname: "Hatake",
         phone: "5559667564",
@@ -151,8 +170,41 @@ const dataInfo = [
         avatar: 'http://www.avotarov.ru/picture/avatar-64/kartinki/932.jpg'
     }
 ]
+const dataTree = [
+    {
+        id: 1,
+        name: "John",
+        surname: "Fris",
+        phone: "5556194558",
+        items: [
+            {
+                id: 2,
+                name: "Steve",
+                surname: "Jobs",
+                phone: "5557503664"
+            },
+            {
+                id: 3,
+                name: "Ram",
+                surname: "Kapoor",
+                phone: "5554063754"
+            },
+            {
+                id: 4,
+                name: "Bill",
+                surname: "Gates",
+                phone: "5556816585"
+            },
+            {
+                id: 5,
+                name: "Moin",
+                surname: "Khan",
+                phone: "5555875011"
+            }]
+    }
 
-app.get('node/api/windowStore', (request, response) => {
+    ]
+/*app.get('node/api/windowStore', (request, response) => {
     const json = {
         total: 20,
         items: [
@@ -280,8 +332,17 @@ app.get('node/api/windowStore', (request, response) => {
         ]
     };
     response.send(json);
+});*/
+app.get('/node/api/treeStore', (req, res) => {
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    res.json({
+        total: dataTree.length,
+        items: dataTree //.slice(startIndex, endIndex)
+    });
 });
-
 app.get('/node/api/newStore', (req, res) => {
     const page = req.query.page;
     const limit = req.query.limit;
@@ -312,14 +373,6 @@ app.get('/node/api/newStore', (req, res) => {
     }
     response.send(json);
 });*/
-
-
-
-
-
-
-
-
 // Запрос данных с пейджингом
 /*
 app.get('/node/api/newStore', (request, response) => {
@@ -341,8 +394,6 @@ app.get('/node/api/newStore', (request, response) => {
     response.send(json);
 });
 */
-
-
 // Добавление/Обновление данных
 app.post('/node/api/newStore', (request, response) => {
     if (!request.body) {
